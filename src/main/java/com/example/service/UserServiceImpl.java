c3c87ceb955280fc3aab54353aab24864301b1eb
ming <Tanyzhong@gmail..com>
2022-02-19 16:45:52 +0800

    v1.0.1

--- src/main/java/com/example/service/UserServiceImpl.java
+++ src/main/java/com/example/service/UserServiceImpl.java
@@ -8,7 +8,7 @@ import com.example.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-@Service("studentService")
+@Service
 public class UserServiceImpl implements UserService {
     @Autowired
     private UserDao userDao;
